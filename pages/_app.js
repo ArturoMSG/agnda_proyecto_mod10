8b7cf75cbfd97085f33e6c3bf8b3c34bb394d5c5
ArturoMSG <amsalazarg1@gmail.com>
2024-12-11 21:45:52 -0600

    correccion de error al utilizar Chakraprovider

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
-  return ( {/* Este parentesis es importante para que se desplegue la informacion, si no se pone y se pone el chakraprovider no aparecera nada en la pantalla*/}
+  return ( 
   
-      <>
-   <ChakraProvider>
-      <Component {...pageProps}/> 
-    </ChakraProvider>
+    <>
+      {/* los parentesis () envolviendo lo que esta dentro del return, es importante para que se desplegue la informacion, si no se pone y se pone el chakraprovider no aparecera nada en la pantalla*/} 
+      <ChakraProvider>
+        <Component {...pageProps}/> 
+      </ChakraProvider>
                 
     </>
-)  
+  )  
 }
