15e379dfbc83c0c6d7043fe35b4908429c3e3fdc
ArturoMSG <amsalazarg1@gmail.com>
2024-12-11 21:39:24 -0600

    se pone el comentario de la pestaña

--- components/BarMenu.js
+++ components/BarMenu.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Head from 'next/head'
 
 const BarMenu = () => {
-  return (
-    <div>BarMenu</div>
-  )
+    return (
+        <> {/* Este div es el contenedor principal de la página.  si no se pone un div  generara un error*/}
+            <Head>
+                <title>Agenda</title> {/* Title sirve para poner el texto en la pestaña del explorador.  */}
+            </Head>
+            <div>BarMenu</div>
+        </>
+    )
 }
 
 export default BarMenu
