cfa5e7ce5be9790e41c6ce2d0d1e458f65feb460
ArturoMSG <amsalazarg1@gmail.com>
2024-12-11 21:15:59 -0600

    puse los parentesis() al return de _app.js y creo que por eso no se deplegaba la informacion

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  
+      <>
+   <ChakraProvider>
+      <h2>funcionando _app</h2>
+      <Component {...pageProps}/> 
+    </ChakraProvider>
+                
+    </>
+)  
 }
