c2c574838790522bdf6dec17f0d32d3b261ab0ee
ArturoMSG <amsalazarg1@gmail.com>
2024-12-11 21:41:44 -0600

    correccion del erro de no se ve nada desplegado al usar Chakraprovider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 
 export default function App({ Component, pageProps }) {
-  return (
+  return ( {/* Este parentesis es importante para que se desplegue la informacion, si no se pone y se pone el chakraprovider no aparecera nada en la pantalla*/}
   
       <>
    <ChakraProvider>
